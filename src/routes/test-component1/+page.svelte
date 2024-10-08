21f817fbab52aafb614d54f0f9f773669ff686da
Matt Wiley <mattwiley+github@fastmail.com>
2024-10-08 16:53:54 -0400

    feat: add button component and click handler to test-component1 page

--- src/routes/test-component1/+page.svelte
+++ src/routes/test-component1/+page.svelte
@@ -1,14 +1,22 @@
 <script>
   // Add any necessary script logic here
+
+	import Button from "$lib/components/Button.svelte";
+
+  function handleClick() {
+    alert("Button clicked!");
+  }
+
 </script>
+
+<div class="test-component1">
+  <h1>Test Component 1</h1>
+  <p>This is the Test Component 1 page.</p>
+  <Button label="Click me" onClick={handleClick} />
+</div>
 
 <style>
   .test-component1 {
     padding: 15px;
   }
 </style>
-
-<div class="test-component1">
-  <h1>Test Component 1</h1>
-  <p>This is the Test Component 1 page.</p>
-</div>
